baea924330626c38811e0577d5c06593b1fd3ab6
archdemon7 <vakylskij@gmail.com>
2022-08-01 19:00:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    stack.push(str[0]);
+    for (let i = 1; i < str.length; i++){
+        if (bracketsConfig.some(e => e[0] == stack[stack.length - 1] && e[1] == str[i])){
+            stack.pop();
+        } else {
+            stack.push(str[i]);
+        };
+    };
+    return stack.length == 0;
 }
